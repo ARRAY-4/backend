dfba4976634c349e997cb4db0d40bda2b0032a55
ryanagungsamudra <ryansamudra67@gmail.com>
2023-03-17 15:03:56 +0700

    feat: add cloudinary

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express();
-const formUpload = require('../middleware/formUpload')
+// const formUpload = require('../middleware/formUpload')
 // const validation = require('../middleware/validation')
+const formUploadOnline = require('../middleware/formUploadOnline')
 
 // import controller
 const usersController = require('../controllers/users.controller.js')
 
 router.get('/', usersController.read)
 router.get('/:id', usersController.readDetail)
-router.post('/', formUpload.single('img_profile'), usersController.create)
-router.patch('/:id', formUpload.single('img_profile'), usersController.update)
+router.post('/', formUploadOnline.single('img_profile'), usersController.create)
+router.patch('/:id', formUploadOnline.single('img_profile'), usersController.update)
 router.delete('/:id', usersController.remove)
 
 module.exports = router
--- src/routes/usersPortfolio.route.js
+++ src/routes/usersPortfolio.route.js
@@ -1,14 +1,15 @@
 // Imports
 const express = require('express')
 const router = express()
-const usersPortfolio = require('../controllers/usersPortfolio.controller') 
-const formUpload = require('../middleware/formUpload')
+const usersPortfolio = require('../controllers/usersPortfolio.controller')
+// const formUpload = require('../middleware/formUpload')
+const formUploadOnline = require('../middleware/formUploadOnline')
 
 // Endpoint Product
 router.get('/', usersPortfolio.read)
 router.get('/:id', usersPortfolio.readDetail)
-router.post('/', formUpload.array('img_portfolio'), usersPortfolio.create)
-router.patch('/:id', formUpload.array('img_portfolio'), usersPortfolio.update)
+router.post('/', formUploadOnline.array('img_portfolio'), usersPortfolio.create)
+router.patch('/:id', formUploadOnline.array('img_portfolio'), usersPortfolio.update)
 router.delete('/:id', usersPortfolio.remove)
 
 // Exports
